56466c1d5764eb27f07c1ba847f008072b512af6
vaticanguy <krystian.szabat@outlook.com>
2023-03-12 21:39:16 +0100

    Laboratoria 1

--- lab_1.py
+++ lab_1.py
@@ -9,13 +9,13 @@ print(f"Zadanie 1.\nSuma głównej przekątnej: {slad}\nWartości głównej prze
 # Zadanie 2
 macierz1 = np.random.normal(size=(5, 5))
 macierz2 = np.random.normal(size=(5, 5))
-macierz3 = macierz1 * macierz2
+macierz3 = np.dot(macierz1, macierz2)
 print(f"\nZadanie 2.\n{macierz3}")
 
 # Zadanie 3
 C = np.random.randint(1, 101, size=20).reshape((-1, 5))
 D = np.random.randint(1, 101, size=20).reshape((-1, 5))
-print(f"\nZadanie 3.\n {C + D}")
+print(f"\nZadanie 3.\n {np.add(C, D)}")
 
 # Zadanie 4
 E = np.array([[1, 2, 3, 4, 5], [0, 0, 1, 1, 0], [0, 0, 1, 1, 1], [0, 0, 0, 1, 1]])
@@ -40,24 +40,24 @@ B = np.array([[1, 0, 0], [0, 2, 0], [0, 0, 3]])
 print(f"\nZadanie 7.\n A * B = \n{A * B}\ndot(A, B) = \n{np.dot(A, B)}")
 # * - mnożenie odpowiadających sobie elementów macierzy A[0, 0] z B[0, 0], A[0, 1] z B[0, 1] itd.
 # dot - mnożenie macierzy - mnożenie wiersza jednej macierzy z kolumną drugiej A[0, :] z B[:, 0], A[1, :] z B[:, 1] itd.
-# Warto wykorzystać funkcję dot do prawidłowego mnożenia dwóch macierzy
+# Warto wykorzystać funkcję dot do prawidłowego mnożenia dwóch macierzy lub mnożenia macierzy przez skalar
 
 # Zadanie 8
 macierz = np.arange(0, 30, dtype=np.int8).reshape((6, 5))
-macierz_widok = np.lib.stride_tricks.as_strided(macierz[0:3, 0:5], shape=(3, 5), strides=macierz.strides)
-print(f"\nZadanie 8.\nMacierz = \n{macierz}\n 3 kolumny z pięciu pierwszych wierszy = \n{macierz_widok}")
+macierz_widok = np.lib.stride_tricks.as_strided(macierz, shape=(3, 4), strides=macierz.strides)
+print(f"\nZadanie 8.\nMacierz = \n{macierz}\n 4 kolumny z 3 pierwszych wierszy = \n{macierz_widok}")
 
 # Zadanie 9
 A = np.ones((2, 3))
 B = np.zeros((2, 3))
 print(f"\nZadanie 9.\nvstack = \n{np.vstack((A, B))}\n stack = \n{np.stack((A, B))}")
 # vstack łączy tablice w pionie
-# stack łączy tablice wzdłuż nowo utworzonej osi
+# stack łączy tablice wzdłuż nowo utworzonej osi, przydaje się np. gdy zamiast łączyć tablice w jedną chcemy stworzyć jedną tablicę składającą się z kilku tablic
 
 # Zadanie 10
-macierz = np.arange(0, 24).reshape((4, 6))
-blok1 = np.lib.stride_tricks.as_strided(macierz[0:2, 0:3], shape=(2, 3), strides=macierz.strides)
-blok2 = np.lib.stride_tricks.as_strided(macierz[0:2, 3:6], shape=(2, 3), strides=macierz.strides)
-blok3 = np.lib.stride_tricks.as_strided(macierz[2:4, 0:3], shape=(2, 3), strides=macierz.strides)
-blok4 = np.lib.stride_tricks.as_strided(macierz[2:4, 3:6], shape=(2, 3), strides=macierz.strides)
-print(f"\nZadanie 10.\nMaksymalne wartości:\nblok_1 = {np.max(blok1)},\tblok_2 = {np.max(blok2)},\tblok_3 = {np.max(blok3)},\tblok_4 = {np.max(blok4)}")
+macierz = np.arange(0, 24, dtype=np.int8).reshape((4, 6))
+strds=macierz.strides
+podzial = np.lib.stride_tricks.as_strided(macierz, shape=(2,2,2,3), strides=(2 * strds[0], 3 * strds[1], strds[0], strds[1]))
+
+# print(f"\n\n{podzial}")
+print(f"\nZadanie 10.\nMaksymalne wartości:\nblok_1 = {np.max(podzial[0][0])},\tblok_2 = {np.max(podzial[0][1])},\tblok_3 = {np.max(podzial[1][0])},\tblok_4 = {np.max(podzial[1][1])}")
